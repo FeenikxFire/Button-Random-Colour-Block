d4091d4e529758d444f5caa4e6353676265ce001
Nic <Nlyness@live.com.au>
2017-08-29 19:33:01 +1000

    Add files via upload

---
+++ RandomButtonColourBlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomButtonColourBlock : MonoBehaviour
+{
+    [Tooltip("Colourblocks you wish the button to choose from. Don't forget to set Alpha, Colour Multiplier & Fade Duration")]
+    public ColorBlock[] myColourBlocks; //Manually pick colours.
+
+    private Button myButton;    //Fills on Start();
+
+    private int random; //Used to decide which colourblock will be chosen.
+
+    // Use this for initialization
+    void Start()
+    {
+        myButton = GetComponent<Button>();
+        RollRandomHighlightColour();
+    }
+
+    public void RollRandomHighlightColour()
+    {
+        random = Random.Range(0, myColourBlocks.Length);
+        myButton.colors = myColourBlocks[random];
+    }
+}
